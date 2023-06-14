82b7ede63603e3b577b0fcd81fe455711d314028
Gustaf <gustaf.ahlner@gmail.com>
2023-06-14 12:25:08 +0200

    add simple dotnet example with SDK

---
+++ DotNet/OpenAIAPIDotnet/Program.cs
@@ -0,0 +1,34 @@
+﻿var apiKey = ""; //insert API key here
+
+if(string.IsNullOrWhiteSpace(apiKey))
+    apiKey = GetNonEmptyInput("No API key found. Insert your API key here:");
+
+var api = new OpenAI_API.OpenAIAPI(apiKey);
+
+var userInput = GetNonEmptyInput("Please write your input to the AI model:");
+
+try
+{
+    var result = await api.Completions.GetCompletion(userInput);
+
+    Console.WriteLine(result);
+}
+catch (Exception e)
+{
+    Console.WriteLine("Exception encountered when calling OpenAI services. Please check API Key validity.");
+    Console.WriteLine(e);
+}
+
+static string GetNonEmptyInput(string input)
+{
+    string? userInput;
+
+    do
+    {
+        Console.WriteLine(input);
+        userInput = Console.ReadLine();
+
+    } while (string.IsNullOrWhiteSpace(userInput));
+
+    return userInput;
+}
